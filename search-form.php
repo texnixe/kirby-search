d9b0cd2af186ed953fa517b0216729899f8db28e
texnixe <info@texniq.de>
2014-11-08 22:11:02 +0100

    Create search-form.php

---
+++ search-form.php
@@ -0,0 +1,6 @@
+<form class="search cf" action="<?php echo url('search') ?>" role="search">
+  <div class="searchbox">
+    <label class="search-label vh" for="search"><?php echo l('search')?></label>
+    <input class="searchword"  type="search" id="search" name="q" placeholder="<?php echo l('search')?>..." >
+  </div>
+</form>
